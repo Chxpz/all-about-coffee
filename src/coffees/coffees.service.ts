ddfe581b0c802f70aca0eedccf2aae465d932575
Chxpz <rpcweb3@gmail.com>
2022-11-29 23:45:08 -0300

    sending frendly error messages

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
@@ -17,8 +17,14 @@ export class CoffeesService {
     }
 
     findOne(id:string){
-        return this.coffees.find(item=> item.id === +id)
-    }
+            const coffee = this.coffees.find(item=> item.id === +id);
+            if(!coffee){
+                throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
+                // both
+                // throw new NotFoundException(`Coffee #${id} not found`);
+            }
+            return coffee;
+        }
 
     create(createCoffeeDto: any){
         this.coffees.push(createCoffeeDto)

b897e15ba2c951255642162a285eb85250e8cd1c
Chxpz <rpcweb3@gmail.com>
2022-12-24 12:39:03 -0300

    validate input data

--- src/coffees/dto/update-coffee.dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto/update-coffee.dto.ts
@@ -1,6 +1,5 @@
-export class UpdateCoffeeDto {}
-export class CreateCoffeeDto {
-    readonly name?: string;
-    readonly brand?:string;
-    readonly flavour?: string[];
-}
+import { PartialType } from "@nestjs/mapped-types";
+import { CreateCoffeeDto } from "../create-coffee.dto/create-coffee.dto";
+
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto){}
+
